3e05876bc883344910840ff4c6443c319c26c6aa
Jason Naylor <jason_naylor@sil.org>
2022-02-04 13:14:08 -0800

    Add SIL modifications
    (cherry-picked from commit 18078dacd3f2f33b4fba40037548c65580c40587)
    (cherry picked from commit 4a1b754b5ef6fe3fdfffefcc83a631a1ece7908f)

--- icu4c/source/common/ucase.cpp
+++ icu4c/source/common/ucase.cpp
@@ -31,6 +31,7 @@
 #include "cmemory.h"
 #include "utrie2.h"
 #include "ucase.h"
+#include "silmods.h" // defines SIL_MODIFICATIONS; including this is an SIL modification.
 
 struct UCaseProps {
     UDataMemory *mem;
@@ -131,9 +132,14 @@ static const uint8_t flagsOffset[256]={
 
 U_CAPI UChar32 U_EXPORT2
 ucase_tolower(UChar32 c) {
+#ifdef SIL_MODIFICATIONS
+    if (SIL_tolower(&c))
+        return c;
+#endif
     uint16_t props=UTRIE2_GET16(&ucase_props_singleton.trie, c);
     if(!UCASE_HAS_EXCEPTION(props)) {
         if(UCASE_IS_UPPER_OR_TITLE(props)) {
+
             c+=UCASE_GET_DELTA(props);
         }
     } else {
@@ -153,6 +159,10 @@ ucase_tolower(UChar32 c) {
 
 U_CAPI UChar32 U_EXPORT2
 ucase_toupper(UChar32 c) {
+#ifdef SIL_MODIFICATIONS
+    if (SIL_toupper(&c))
+        return c;
+#endif
     uint16_t props=UTRIE2_GET16(&ucase_props_singleton.trie, c);
     if(!UCASE_HAS_EXCEPTION(props)) {
         if(UCASE_GET_TYPE(props)==UCASE_LOWER) {
@@ -175,6 +185,10 @@ ucase_toupper(UChar32 c) {
 
 U_CAPI UChar32 U_EXPORT2
 ucase_totitle(UChar32 c) {
+#ifdef SIL_MODIFICATIONS
+    if (SIL_totitle(&c))
+        return c;
+#endif
     uint16_t props=UTRIE2_GET16(&ucase_props_singleton.trie, c);
     if(!UCASE_HAS_EXCEPTION(props)) {
         if(UCASE_GET_TYPE(props)==UCASE_LOWER) {
@@ -581,6 +595,11 @@ U_NAMESPACE_END
 /** @return UCASE_NONE, UCASE_LOWER, UCASE_UPPER, UCASE_TITLE */
 U_CAPI int32_t U_EXPORT2
 ucase_getType(UChar32 c) {
+#ifdef SIL_MODIFICATIONS
+    int32_t result = SIL_ucase_getType(c);
+    if (result >= 0)
+        return result;
+#endif
     uint16_t props=UTRIE2_GET16(&ucase_props_singleton.trie, c);
     return UCASE_GET_TYPE(props);
 }
